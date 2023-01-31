856196169bf19f85e86fdd2bd72d059e4b054113
Macko++ <mackomaxo@duck.com>
2023-01-31 10:12:42 -0500

    Made it responsive, made player names editable

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,22 +6,22 @@
 
 <section class="grid grid-cols-5 grid-rows-3">
 
-	<div class="avatar col-start-2 col-end-3 row-start-3">
+	<div class="avatar col-start-2 row-start-3 flex justify-center">
 	  	<div class=" w-20 rounded-lg">
 			<img src="/Player 1.png " alt="stock avatar"/>
 	  	</div>
 	</div>
 	<div class="row-start-4 col-start-2 flex justify-center">
-		<p class="">Player 1</p>
+		<p contenteditable="true">Player 1</p>
 	</div>
 	
-	<div class="avatar col-start-4 row-start-4 row-end-3">
+	<div class="avatar col-start-4 row-start-4 row-end-3 flex justify-center">
 		<div class=" w-20 rounded-lg">
 			<img src="/Player 2.png " alt="stock avatar"/>
 		</div>
 	</div>
 	<div class="row-start-4 col-start-4 flex justify-center">
-		<p class="">Player 2</p>
+		<p contenteditable="true">Player 2</p>
 	</div>
 
 	
