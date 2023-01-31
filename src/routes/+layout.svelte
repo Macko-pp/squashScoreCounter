5d0f32e27425a343b0504e958a9562a962a6e9ec
Macko++ <mackomaxo@duck.com>
2023-01-31 10:56:46 -0500

    Added page Title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,10 @@
 	import '../app.postcss';
 </script>
 
+<svelte:head>
+	<title>Score Counter</title>
+</svelte:head>
+
 <div class="navbar shadow-lg flex items-center justify-center font-bold">
 	Squash Score Counter
 </div>
